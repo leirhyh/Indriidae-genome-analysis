099b3cb229edb36ce9a9ca943c3b830a2fd8f9f7
leirhyh <runhua.lei@gmail.com>
2016-12-07 15:36:32 -0600

    Update ordered_phy_sortindriidae.py

--- ordered_phy_sortindriidae.py
+++ ordered_phy_sortindriidae.py
@@ -3,6 +3,7 @@ import sys
 import os
 import argparse
 
+"""This script was for sorting the phylip file by taxon developed by Ryan Culligan, slightly modified for different dataset"""
 
 parser = argparse.ArgumentParser()
 
